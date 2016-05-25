b2d9b633a7b6ed6a458c99c3b4fad7c923655609
Eduard Albu <eduard.albu@gmail.com>
2016-05-25 13:59:13 +0300

    hide show plus button on select action

---
+++ app/src/main/java/com/softranger/bayshopmf/adapter/FirstStepAdapter.java
@@ -0,0 +1,81 @@
+package com.softranger.bayshopmf.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.softranger.bayshopmf.R;
+import com.softranger.bayshopmf.model.InStockItem;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Eduard Albu on 5/25/16, 05, 2016
+ * for project BayShop MF
+ * email eduard.albu@gmail.com
+ */
+public class FirstStepAdapter extends RecyclerView.Adapter<FirstStepAdapter.ViewHolder> {
+
+    private ArrayList<InStockItem> mInStockItems;
+    private OnItemClickListener mOnItemClickListener;
+
+    public FirstStepAdapter(ArrayList<InStockItem> inStockItems) {
+        mInStockItems = new ArrayList<>();
+        mInStockItems.addAll(inStockItems);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        mOnItemClickListener = onItemClickListener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.build_parcel_first_step_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mInStockItem = mInStockItems.get(position);
+        holder.mMfLabel.setText(holder.mInStockItem.getParcelId());
+        holder.mNameLabel.setText(holder.mInStockItem.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mInStockItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        final TextView mMfLabel;
+        final TextView mNameLabel;
+        final TextView mPriceLabel;
+        final TextView mWeightLabel;
+        final ImageButton mDeleteButton;
+        InStockItem mInStockItem;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mMfLabel = (TextView) itemView.findViewById(R.id.buildFirstStepMfIdLabel);
+            mNameLabel = (TextView) itemView.findViewById(R.id.buildFirstStepNameLabel);
+            mPriceLabel = (TextView) itemView.findViewById(R.id.buildFirstStepPriceLabel);
+            mWeightLabel = (TextView) itemView.findViewById(R.id.buildFirstStepWeghtLabel);
+            mDeleteButton = (ImageButton) itemView.findViewById(R.id.buildFirstDeleteButton);
+            mDeleteButton.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mOnItemClickListener != null) {
+                mOnItemClickListener.onDeleteClick(mInStockItem, getAdapterPosition());
+            }
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onDeleteClick(InStockItem inStockItem, int position);
+    }
+}
--- app/src/main/java/com/softranger/bayshopmf/ui/instock/BuildParcelFirstStep.java
+++ app/src/main/java/com/softranger/bayshopmf/ui/instock/BuildParcelFirstStep.java
@@ -3,17 +3,22 @@ package com.softranger.bayshopmf.ui.instock;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.softranger.bayshopmf.R;
+import com.softranger.bayshopmf.ui.MainActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BuildParcelFirstStep extends Fragment {
 
+    private MainActivity mActivity;
 
     public BuildParcelFirstStep() {
         // Required empty public constructor
@@ -24,7 +29,16 @@ public class BuildParcelFirstStep extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_build_parcel_first_step, container, false);
+        View view = inflater.inflate(R.layout.fragment_build_parcel_first_step, container, false);
+        mActivity = (MainActivity) getActivity();
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.buildFirstStepItemsList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
+
+        String listItems = getString(R.string.list_items);
+        mActivity.setToolbarTitle(listItems, true);
+
+        return view;
     }
 
 }
