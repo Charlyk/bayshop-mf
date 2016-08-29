186da5b2c9e1d9e872cf45003c9f03b4852302fe
Eduard Albu <eduard.albu@gmail.com>
2016-08-29 16:30:17 +0300

    Added chat animation aka Facebook Messenger

--- app/src/main/java/com/softranger/bayshopmf/adapter/ChatAdapter.java
+++ app/src/main/java/com/softranger/bayshopmf/adapter/ChatAdapter.java
@@ -1,9 +1,13 @@
 package com.softranger.bayshopmf.adapter;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.os.Handler;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewPropertyAnimator;
 import android.widget.TextView;
 
 import com.softranger.bayshopmf.R;
@@ -20,6 +24,8 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
 
     private ArrayList<ChatMessage> mChatMessages;
     private OnMessageClickListener mOnMessageClickListener;
+
+    private static final float DATAILS_H = 49f;
 
     private static final int INCOMING = 0, OUTGOING = 1, UNKNOWN = -1;
 
@@ -89,15 +95,22 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
             mMessageLabel = (TextView) itemView.findViewById(R.id.incomingMessageTextLabel);
             mMessageLabel.setOnClickListener(this);
             mMessageLabel.setOnLongClickListener(this);
+            mDateLabel.setVisibility(View.GONE);
             mAuthorLabel = (TextView) itemView.findViewById(R.id.incomingMessageAuthorLabel);
+            mAuthorLabel.setVisibility(View.GONE);
         }
 
         @Override
         public void onClick(View v) {
             mMessageLabel.setSelected(!mMessageLabel.isSelected());
             boolean isSelected = mMessageLabel.isSelected();
-            mDateLabel.setVisibility(isSelected ? View.VISIBLE : View.GONE);
-            mAuthorLabel.setVisibility(isSelected ? View.VISIBLE : View.GONE);
+
+            if (isSelected) {
+                expandDetails(mAuthorLabel, mDateLabel, true);
+            } else {
+                expandDetails(mAuthorLabel, mDateLabel, false);
+            }
+
             if (mOnMessageClickListener != null) {
                 mOnMessageClickListener.onMessageClicked(mChatMessage, getAdapterPosition());
             }
@@ -111,6 +124,62 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
             }
             return false;
         }
+    }
+
+    /**
+     * Expand or collapse details texts
+     * it can expand and collapse for now just two text views
+     * @param first text view to be animated
+     * @param second text view to be animated
+     * @param expand if true texts will be expanded and set as visible else texts will
+     *               be collapsed and set as gone
+     */
+    private void expandDetails(final TextView first, final TextView second, final boolean expand) {
+        final float fromValue = expand ? 0 : first.getMeasuredHeight();
+        final float toValue = expand ? DATAILS_H : 0;
+        ValueAnimator animator = ValueAnimator.ofFloat(fromValue, toValue);
+        animator.setDuration(300);
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float height = (float) animation.getAnimatedValue();
+                first.setHeight((int) height);
+                second.setHeight((int) height);
+            }
+        });
+
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                if (expand) {
+                    first.setVisibility(View.VISIBLE);
+                    second.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (!expand) {
+                    first.setVisibility(View.GONE);
+                    second.setVisibility(View.GONE);
+                }
+                // set default height
+                first.setHeight((int) (expand ? toValue : fromValue));
+                second.setHeight((int) (expand ? toValue : fromValue));
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        animator.start();
     }
 
     public interface OnMessageClickListener {
