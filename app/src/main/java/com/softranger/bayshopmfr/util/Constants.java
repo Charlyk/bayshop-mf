18ca3127d76a7d29e07d91e33730497fa8238ea9
Eduard Albu <eduard.albu@gmail.com>
2016-11-04 11:56:15 +0200

    added russian language

--- app/src/main/java/com/softranger/bayshopmfr/util/Constants.java
+++ app/src/main/java/com/softranger/bayshopmfr/util/Constants.java
@@ -51,7 +51,7 @@ public class Constants {
     public static class Api {
         public static final String TRACK_URL = "https://gdeposylka.ru/api/v4/";
         public static final String BASE_URL = "http://bayshop.com";
-        //        public static final String BASE_API_URL = "http://bayshop.com/api/";
+//        public static final String BASE_API_URL = "http://bayshop.com/api/";
         public static final String BASE_API_URL = "http://bay-test.tk/api/";
         private static final String AUTH = "auth/";
         private static final String MEMBER = "member/";
